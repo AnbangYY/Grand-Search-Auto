87bf6b22ea29f3fa0aa4d8818ffb72fbe53d3ab0
Kaiyu Zhang <u6746390@anu.edu.au>
2020-10-08 04:40:05 +1100

    “first commit”

--- app/src/main/java/com/example/a2100assignment/MainActivity.java
+++ app/src/main/java/com/example/a2100assignment/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.a2100assignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] mStrs = {"bmw", "qq", "audi", "benz"};
+    private SearchView mySearchView;
+    private ListView myListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mySearchView = (SearchView) findViewById(R.id.searchView);
+        myListView = (ListView) findViewById(R.id.listView);
+        myListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mStrs));
+        myListView.setTextFilterEnabled(true);
+        mySearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (!TextUtils.isEmpty(newText)){
+                    myListView.setFilterText(newText);
+                }else{
+                    myListView.clearTextFilter();
+                }
+                return false;
+            }
+        });
+
     }
 }

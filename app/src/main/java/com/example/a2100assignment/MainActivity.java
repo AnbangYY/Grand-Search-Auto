a3a7c7a42adf2291d078db20ceff93e4c187367b
Kaiyu Zhang <u6746390@anu.edu.au>
2020-10-09 17:56:36 +1100

    “second commit”

--- app/src/main/java/com/example/a2100assignment/MainActivity.java
+++ app/src/main/java/com/example/a2100assignment/MainActivity.java
@@ -7,25 +7,32 @@ import android.text.TextUtils;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private String[] mStrs = {"bmw", "qq", "audi", "benz"};
     private SearchView mySearchView;
     private ListView myListView;
-
+    private ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mySearchView = (SearchView) findViewById(R.id.searchView);
         myListView = (ListView) findViewById(R.id.listView);
-        myListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mStrs));
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, mStrs);
+        myListView.setAdapter(adapter);
         myListView.setTextFilterEnabled(true);
         mySearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                return false;
+                if (mStrs.equals(query)) {
+                    adapter.getFilter().filter(query);
+                }else {
+                    Toast.makeText(MainActivity.this,"the car that you are looking for is not in the list",Toast.LENGTH_LONG).show();
+                }
+                return true;
             }
             @Override
             public boolean onQueryTextChange(String newText) {
@@ -33,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
                     myListView.setFilterText(newText);
                 }else{
                     myListView.clearTextFilter();
+                    System.out.println("dsflad");
                 }
                 return false;
             }

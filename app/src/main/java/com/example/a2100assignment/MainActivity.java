c0c023656dbfe0cd18ea00bc600a42b1bb140514
Kaiyu Zhang <u6746390@anu.edu.au>
2020-10-11 00:53:43 +1100

    “fivth commit”

--- app/src/main/java/com/example/a2100assignment/MainActivity.java
+++ app/src/main/java/com/example/a2100assignment/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,25 +22,15 @@ public class MainActivity extends AppCompatActivity {
      SearchView mySearchView;
      ListView myListView;
      ArrayAdapter adapter;
-     ArrayList list;
+     ArrayList CUS_LIST_TYPE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        list = new ArrayList<>();
-        list.add("Apple");
-        list.add("Banana");
-        list.add("Pineapple");
-        list.add("Orange");
-        list.add("Mango");
-        list.add("Grapes");
-        list.add("Lemon");
-        list.add("Melon");
-        list.add("Watermelon");
-        list.add("Papaya");
+
         mySearchView = (SearchView) findViewById(R.id.searchView);
         myListView = (ListView) findViewById(R.id.listView);
-        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, list);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
         myListView.setAdapter(adapter);
         myListView.setTextFilterEnabled(true);
         mySearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -43,22 +38,21 @@ public class MainActivity extends AppCompatActivity {
             public boolean onQueryTextSubmit(String query) {
                 if (list.equals(query)) {
                     adapter.getFilter().filter(query);
-                }else {
-                    Toast.makeText(MainActivity.this,"the car that you are looking for is not in the list",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "the car that you are looking for is not in the list", Toast.LENGTH_LONG).show();
                 }
                 return true;
             }
+
             @Override
             public boolean onQueryTextChange(String newText) {
-                if (!TextUtils.isEmpty(newText)){
+                if (!TextUtils.isEmpty(newText)) {
                     myListView.setFilterText(newText);
-                }else{
+                } else {
                     myListView.clearTextFilter();
                     System.out.println("dsflad");
                 }
                 return false;
             }
         });
-
-    }
-}
+    }}
